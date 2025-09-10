7c3bab22863f9c83de4ffb7c00dfbf2c5484f3da
nehir_jy8v0pp <nehir_jy8v0pp@DESKTOP-FE48GM4>
2025-09-10 13:58:00 +0300

    Deleted comment line

--- WhatToDo/Program.cs
+++ WhatToDo/Program.cs
@@ -6,8 +6,6 @@ builder.Services.AddControllers();
 // Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
 builder.Services.AddOpenApi();
 
-// Added a new comment
-
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
